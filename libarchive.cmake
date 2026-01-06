53fab8517377b5df889cf090a7afe630f0a2cc48
Michael Hitchens <hitchens@google.com>
2026-01-06 17:58:43 -0500

    Simplify libarchive.cmake

--- libarchive.cmake
+++ libarchive.cmake
@@ -14,64 +14,11 @@
 # limitations under the License.
 #
 
-if(WIN32)
-    set(CMAKE_PREFIX_PATH_PRE ${CMAKE_PREFIX_PATH})
-    set(CMAKE_PREFIX_PATH "${CMAKE_SOURCE_DIR}/prebuild/libarchive/")
-    add_compile_definitions(LIBARCHIVE_STATIC)
+# Find libarchive. Defines the LibArchive::LibArchive target
+
+# Prebuilts are only offered for Windows
+if (NOT WIN32)
+    add_subdirectory(third_party/libarchive)
+    add_library(LibArchive::LibArchive ALIAS archive_static)
+    return()
 endif()
-
-find_package(LibArchive QUIET)
-
-if(LibArchive_FOUND)
-    if(WIN32)
-        set(CMAKE_PREFIX_PATH "${CMAKE_SOURCE_DIR}/prebuild/zlib")
-        find_package(ZLIB)
-        message("ZLIB_LIBRARIES is " ${ZLIB_LIBRARIES})
-    endif()
-else()
-    # For Windows we need to build the zlib
-    if(WIN32)
-        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH_PRE})
-        # Enable find_package uses of <PackageName>_ROOT variables.
-        cmake_policy(SET CMP0074 NEW)
-        set(ZLIB_ROOT ${THIRDPARTY_DIRECTORY}/zlib)
-        if(EXISTS "${ZLIB_ROOT}/CMakeLists.txt")
-            include_directories("${ZLIB_ROOT}")
-            add_subdirectory(${ZLIB_ROOT} third_party/zlib)
-            find_package(ZLIB)
-            if(ZLIB_FOUND)
-                set(ZLIB_INCLUDE_DIR
-                    ${CMAKE_BINARY_DIR}/third_party/zlib
-                    ${ZLIB_INCLUDE_DIR}
-                )
-                message("ZLIB_INCLUDE_DIR is " ${ZLIB_INCLUDE_DIR})
-            else()
-                message(FATAL_ERROR "did not find zlib")
-            endif()
-        endif()
-    endif()
-
-    set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_TEST OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_OPENSSL OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_LIBB2 OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_LZ4 OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_LZMA OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_ZSTD OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_BZip2 OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_CNG OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_TAR OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_CPIO OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_CAT OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_ACL OFF CACHE INTERNAL "" FORCE)
-    set(ENABLE_INSTALL OFF CACHE INTERNAL "" FORCE)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/libarchive)
-    set(LibArchive_LIBRARIES archive_static)
-    set(LibArchive_INCLUDE_DIRS ${THIRDPARTY_DIRECTORY}/libarchive/libarchive/)
-    add_compile_definitions(LIBARCHIVE_STATIC=ON)
-    include_directories(${THIRDPARTY_DIRECTORY}/libarchive/libarchive/)
-endif()
-
-include_directories(LibArchive_INCLUDE_DIRS)
-message("LibArchive_LIBRARIES is " ${LibArchive_LIBRARIES})
-message("LibArchive_INCLUDE_DIRS is " ${LibArchive_INCLUDE_DIRS})
